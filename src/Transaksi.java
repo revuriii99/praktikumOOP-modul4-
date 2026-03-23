bc77774d54d6f67873eae8e9b84a2edfb9d27be7
revuriii99 <reviwilerii@gmail.com>
2026-03-23 20:15:13 +0700

    finish class Transaksi

--- src/Transaksi.java
+++ src/Transaksi.java
@@ -1,2 +1,28 @@
 public class Transaksi {
+    String namaPelanggan;
+    String jenisTransaksi;
+    double jumlah;
+    double cashback;
+    double saldoSebelum;
+    double saldoSesudah;
+
+    Transaksi(String namaPelanggan, String jenisTransaksi, double jumlah, double cashback, double saldoSebelum, double saldoSesudah) {
+        this.namaPelanggan  = namaPelanggan;
+        this.jenisTransaksi = jenisTransaksi;
+        this.jumlah         = jumlah;
+        this.cashback       = cashback;
+        this.saldoSebelum   = saldoSebelum;
+        this.saldoSesudah   = saldoSesudah;
+    }
+
+    void tampilStruk() {
+        System.out.println("===== STRUK TRANSAKSI =====");
+        System.out.println("Nama Pelanggan : " + namaPelanggan);
+        System.out.println("Jenis          : " + jenisTransaksi);
+        System.out.println("Jumlah         : " + jumlah);
+        System.out.println("Cashback       : " + cashback);
+        System.out.println("Saldo Sebelum  : " + saldoSebelum);
+        System.out.println("Saldo Sesudah  : " + saldoSesudah);
+        System.out.println("===========================");
+    }
 }
